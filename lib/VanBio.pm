8673b1da0be6aa5d7eb1331a2a1c2d7361b0661b
Luke Closs <lukec@perhaps.(none)>
2009-02-10 01:39:45 -0800

    pull content from the wiki instead of hard-coding it

--- lib/VanBio.pm
+++ lib/VanBio.pm
@@ -5,15 +5,18 @@ use Template;
 use File::Path qw/rmtree mkpath/;
 use Fatal qw/rmtree mkpath/;
 use Carp qw/croak/;
+use Socialtext::WikiObject;
 
 sub new {
     my $class = shift;
     my $self = { @_ };
 
-    for (qw/output_dir static_dir template_dir/) {
+    for (qw/output_dir static_dir template_dir rester/) {
         croak "$_ is required!" unless $self->{$_};
     }
     bless $self, $class;
+
+    $self->{rester}->accept('text/html');
     return $self;
 }
 
@@ -45,17 +48,23 @@ sub copy_static {
 
 sub render_homepage {
     my $self = shift;
-    $self->_render_page( 'index' );
+
+    my $content = $self->_get_page_html('home page');
+    $self->_render_page( 'index', { content => $content } );
 }
 
 sub render_news {
     my $self = shift;
-    $self->_render_page( 'news' );
+
+    my $content = $self->_get_page_html('news page');
+    $self->_render_page( 'news', { content => $content } );
 }
 
 sub render_join {
     my $self = shift;
-    $self->_render_page( 'join' );
+
+    my $content = $self->_get_page_html('join page');
+    $self->_render_page( 'join', { content => $content } );
 }
 
 sub render_photos {
@@ -70,7 +79,24 @@ sub render_press {
 
 sub render_links {
     my $self = shift;
-    $self->_render_page( 'links' );
+
+    $self->{rester}->accept('text/x.socialtext-wiki');
+    my $page = Socialtext::WikiObject->new(
+        rester => $self->{rester},
+        page => 'links page',
+    );
+    my $links = [];
+    my $link_table = $page->{links}{table};
+    shift @$link_table; # pop off the header row
+    for my $row (@$link_table) {
+        push @$links, {
+            href => $row->[0],
+            name => $row->[1],
+            desc => $row->[2],
+        };
+    }
+
+    $self->_render_page( 'links', { links => $links } );
 }
 
 sub _render_page {
@@ -87,4 +113,10 @@ sub _render_page {
         || die $template->error();
 }
 
+sub _get_page_html {
+    my $self = shift;
+    my $page = shift;
+    $self->{rester}->accept('text/html');
+    return $self->{rester}->get_page($page);
+}
 1;
